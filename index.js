1167c767150c7349ae89a2f9aeda3dca8174f1fd
Kirill <mox1toqq@gmail.com>
2021-10-08 02:06:46 +0300

    fix error db lost connect

--- index.js
+++ index.js
@@ -2,48 +2,17 @@ import { Telegraf } from "telegraf";
 import mysql from "mysql";
 
 let connection = null;
-
-function handleDisconnect() {
+function dbCreateConnection() {
   connection = mysql.createConnection({
     host: "141.8.196.54",
     user: "grek7771_testbd",
     password: "testbd",
     database: "grek7771_testbd",
-  }); // Recreate the connection, since
-  // the old one cannot be reused.
-
-  connection.connect(function (err) {
-    // The server is either down
-    if (err) {
-      // or restarting (takes a while sometimes).
-      console.log("error when connecting to db:", err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    } // to avoid a hot loop, and to allow our node script to
-  }); // process asynchronous requests in the meantime.
-  // If you're also serving http, display a 503 error.
-  connection.on("error", function (err) {
-    console.log("db error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      // Connection to the MySQL server is usually
-      handleDisconnect(); // lost due to either server restart, or a
-    } else {
-      // connnection idle timeout (the wait_timeout
-      throw err; // server variable configures this)
-    }
   });
 }
-
-handleDisconnect();
+dbCreateConnection();
 
 const bot = new Telegraf("1820848598:AAFodpdUPNFB3fT2rDZkGrkuDcPu7Ax-QGo");
-
-// bot.command("quit", (ctx) => {
-//   // Explicit usage
-//   ctx.telegram.leaveChat(ctx.message.chat.id);
-
-//   // Using context shortcut
-//   ctx.leaveChat();
-// });
 
 bot.on("text", (ctx) => {
   let chatId = ctx.message.chat.id;
@@ -63,11 +32,20 @@ bot.on("text", (ctx) => {
       "SELECT * FROM affairs WHERE ChatId = " + chatId + " ORDER BY Id";
 
     connection.query(query, function (error, results, fields) {
-      if (error) throw error;
-
-      results.map((element) => {
-        ctx.telegram.sendMessage(chatId, element.Id + ". " + element.Name);
-      });
+      if (error) {
+        console.log(error);
+        ctx.telegram.sendMessage(
+          chatId,
+          "Возникла ошибка, попробуйте еще раз!"
+        );
+        dbCreateConnection();
+      } else {
+        let allBusiness = "";
+        results.map((element) => {
+          allBusiness += element.Id + ". " + element.Name + "\n";
+        });
+        ctx.telegram.sendMessage(chatId, allBusiness);
+      }
     });
   }
 
@@ -129,23 +107,6 @@ bot.on("text", (ctx) => {
     });
   }
 });
-
-// bot.on("callback_query", (ctx) => {
-//   // Explicit usage
-//   ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
-
-//   // Using context shortcut
-//   ctx.answerCbQuery();
-// });
-
-// bot.on("inline_query", (ctx) => {
-//   const result = [];
-//   // Explicit usage
-//   ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result);
-
-//   // Using context shortcut
-//   ctx.answerInlineQuery(result);
-// });
 
 bot.launch();
 
