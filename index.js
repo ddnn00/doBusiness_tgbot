d47c35d338f1848dfb842739d3b8981610cd123e
Kirill <mox1toqq@gmail.com>
2021-10-13 01:45:45 +0300

    bugfix

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 import { Telegraf } from "telegraf";
 import mysql from "mysql";
 
-let connection = null;
-function dbCreateConnection() {
-  connection = mysql.createConnection({
-    host: "141.8.196.54",
-    user: "grek7771_testbd",
-    password: "testbd",
-    database: "grek7771_testbd",
-  });
+const dbConfig = {
+  host: "141.8.196.54",
+  user: "grek7771_testbd",
+  password: "testbd",
+  database: "grek7771_testbd",
 }
-dbCreateConnection();
+
+let connection = null;
+function dbConnectionHandler() {
+  connection = mysql.createConnection(dbConfig);
+
+  connection.connect(function(error) {
+    if(error) {
+      console.log('Database connection failed:', error);
+      setTimeout(dbConnectionHandler, 2000);
+    } else {
+      console.log('Database connection successful!')
+    }
+  });   
+
+  connection.on('error', function(error) {
+    console.log('DB error:', error);
+    if(error.code === 'PROTOCOL_CONNECTION_LOST') {
+      dbConnectionHandler();
+    } else {                
+      throw err;                            
+    }
+  })
+}
+dbConnectionHandler();
 
 const bot = new Telegraf("1820848598:AAFodpdUPNFB3fT2rDZkGrkuDcPu7Ax-QGo");
 
 bot.on("text", (ctx) => {
   let chatId = ctx.message.chat.id;
 
-  if (ctx.message.text == "help") {
-    ctx.telegram.sendMessage(
-      chatId,
-      "getAllBusiness - Получить все дела\n" +
-        "getLastLogByBusinessId [Номер дела] - Получить последнюю запись по делу\n" +
-        "addBusiness [Название дела] - Добавить дело\n" +
-        "addLogForBusiness [Описание] [Номер дела] - Добавить запись к делу"
-    );
-  }
+  // if (ctx.message.text == "help") {
+  //   ctx.telegram.sendMessage(
+  //     chatId,
+  //     "getAllBusiness - Получить все дела\n" +
+  //       "getLastLogByBusinessId [Номер дела] - Получить последнюю запись по делу\n" +
+  //       "addBusiness [Название дела] - Добавить дело\n" +
+  //       "addLogForBusiness [Описание] [Номер дела] - Добавить запись к делу"
+  //   );
+  // }
 
   if (ctx.message.text == "getAllBusiness") {
     let query =
@@ -38,7 +58,7 @@ bot.on("text", (ctx) => {
           chatId,
           "Возникла ошибка, попробуйте еще раз!"
         );
-        dbCreateConnection();
+        dbConnectionHandler();
       } else {
         let allBusiness = "";
         results.map((element) => {
@@ -49,63 +69,63 @@ bot.on("text", (ctx) => {
     });
   }
 
-  if (ctx.message.text.indexOf("getLastLogByBusinessId") != -1) {
-    let affairId = ctx.message.text.split(" ")[1];
+  // if (ctx.message.text.indexOf("getLastLogByBusinessId") != -1) {
+  //   let affairId = ctx.message.text.split(" ")[1];
 
-    let query =
-      "SELECT * FROM affairsLog WHERE AffairId = " + affairId + " ORDER BY Id";
+  //   let query =
+  //     "SELECT * FROM affairsLog WHERE AffairId = " + affairId + " ORDER BY Id";
 
-    connection.query(query, function (error, results, fields) {
-      if (error) throw error;
+  //   connection.query(query, function (error, results, fields) {
+  //     if (error) throw error;
 
-      if (results.length == 0) {
-        ctx.telegram.sendMessage(
-          chatId,
-          "По запрашиваему делу нет информации!"
-        );
-      } else {
-        ctx.telegram.sendMessage(
-          chatId,
-          results[results.length - 1].Description
-        );
-      }
-    });
-  }
+  //     if (results.length == 0) {
+  //       ctx.telegram.sendMessage(
+  //         chatId,
+  //         "По запрашиваему делу нет информации!"
+  //       );
+  //     } else {
+  //       ctx.telegram.sendMessage(
+  //         chatId,
+  //         results[results.length - 1].Description
+  //       );
+  //     }
+  //   });
+  // }
 
-  if (ctx.message.text.indexOf("addBusiness") != -1) {
-    let affairName = ctx.message.text.split(" ")[1];
+  // if (ctx.message.text.indexOf("addBusiness") != -1) {
+  //   let affairName = ctx.message.text.split(" ")[1];
 
-    let query =
-      "INSERT affairs(Name, ChatId) VALUES ('" +
-      affairName +
-      "', " +
-      chatId +
-      ");";
+  //   let query =
+  //     "INSERT affairs(Name, ChatId) VALUES ('" +
+  //     affairName +
+  //     "', " +
+  //     chatId +
+  //     ");";
 
-    connection.query(query, function (error, results, fields) {
-      if (error) throw error;
+  //   connection.query(query, function (error, results, fields) {
+  //     if (error) throw error;
 
-      ctx.telegram.sendMessage(chatId, "Дело добавлено!");
-    });
-  }
+  //     ctx.telegram.sendMessage(chatId, "Дело добавлено!");
+  //   });
+  // }
 
-  if (ctx.message.text.indexOf("addLogForBusiness") != -1) {
-    let description = ctx.message.text.split(" ")[1];
-    let affairId = ctx.message.text.split(" ")[2];
+  // if (ctx.message.text.indexOf("addLogForBusiness") != -1) {
+  //   let description = ctx.message.text.split(" ")[1];
+  //   let affairId = ctx.message.text.split(" ")[2];
 
-    let query =
-      "INSERT affairsLog(Description, AffairId) VALUES ('" +
-      description +
-      "', " +
-      affairId +
-      ");";
+  //   let query =
+  //     "INSERT affairsLog(Description, AffairId) VALUES ('" +
+  //     description +
+  //     "', " +
+  //     affairId +
+  //     ");";
 
-    connection.query(query, function (error, results, fields) {
-      if (error) throw error;
+  //   connection.query(query, function (error, results, fields) {
+  //     if (error) throw error;
 
-      ctx.telegram.sendMessage(chatId, "Описание добавлено!");
-    });
-  }
+  //     ctx.telegram.sendMessage(chatId, "Описание добавлено!");
+  //   });
+  // }
 });
 
 bot.launch();
